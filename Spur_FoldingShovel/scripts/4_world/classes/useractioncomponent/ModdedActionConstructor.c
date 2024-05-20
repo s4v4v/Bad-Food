b25da812affd5ba53aea00aa0f13b0731155f7b4
s4v4v <127071181+s4v4v@users.noreply.github.com>
2024-05-20 07:36:35 -0400

    Delete Spur_FoldingShovel directory

--- Spur_FoldingShovel/scripts/4_world/classes/useractioncomponent/ModdedActionConstructor.c
+++
@@ -1,9 +0,0 @@
-modded class ActionConstructor
-{
-	override void RegisterActions(TTypenameArray actions)
-	{
-		super.RegisterActions(actions)
-		actions.Insert(ActionFold_FoldingShovel);
-		actions.Insert(ActionUnFold_FoldingShovel);
-	};
-};
